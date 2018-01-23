e69cb63443589990328e6336168dc1f9e252e385
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-23 20:05:45 +0100

    pokemon added

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-$.ajax({
-    url: "pokemons.json",
-    type: "get",
-    dataType: "json",
-    success: function (data) {
-        console.log(data);
-        for (var i in data) {
-            console.log(i)
-        }
-    },
-    error: function () {
-        alert('oops');
-    }
-}
-)
+
 window.onload = function(){ 
     $("form[name=form]").submit(function () {
         var pokemon = $(this).find(':text[name="pokemon"]').val();
-    console.log(pokemon);
+        console.log(pokemon);
+        $.ajax({
+            url: "pokemons.json",
+            type: "get",
+            dataType: "json",
+            success: function (data) {
+                for (var i in data) {
+                    if (data[i].name===pokemon){  
+                     ok = pokemon.toLowerCase();                   	
+                    $( ".blackborder" ).replaceWith( "<img src='http://img.pokemondb.net/artwork/"+ok+".jpg' class='blackborder'>" );
+                    }
+                     else if(i===pokemon){
+                        namel = data[i].name;
+                        ok = namel.toLowerCase();
+                        $( ".blackborder" ).replaceWith( "<img src='http://img.pokemondb.net/artwork/"+ok+".jpg'" );
+                        $( ".blackborder" ).addClass("blackborder")
+                    }
+                    };
+                
+            },
+            error: function () {
+                alert('oops');
+            }
+        })
     return false;
     })
 }
