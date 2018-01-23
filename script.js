14550e903dab51850324f37bbc9eb55254fc76ab
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-23 20:58:04 +0100

    ajax outside of a function

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-function replaceWith(name,type) {
+$.ajax({
+    url: "pokemons.json",
+    type: "get",
+    dataType: "json",
+    data :"data",
+    success: "success"
+})
+function replaceWith(name, type) {
     $(".blackborder").replaceWith("<img src='http://img.pokemondb.net/artwork/" + name + ".jpg'>");
     $("img").addClass("blackborder");
     $("img").addClass("bigradius");
-    $(".type").html(name+type);
+    $(".type").html(name + type);
 }
-window.onload = function () {
-    $("form[name=form]").submit(function () {
-        var pokemon = $(this).find(':text[name="pokemon"]').val();
-        console.log(pokemon);
-        $.ajax({
-            url: "pokemons.json",
-            type: "get",
-            dataType: "json",
-            success: function (data) {
-
-                for (var i in data) {
-                    if (data[i].name === pokemon) {
-                        lowercasepokemon = pokemon.toLowerCase();
-                        type = data[i].type;
-                        replaceWith(lowercasepokemon,type)
-                    }
-
-                    else if (i === pokemon) {
-                        namel = data[i].name;
-                        type = data[i].type;
-                        lowercasepokemon = namel.toLowerCase();
-                        replaceWith(lowercasepokemon,type)
-                    }
-
-                };
-            },
-            error: function () {
-                alert('oops');
-            }
-        })
-        return false;
-    })
-}
+$("form[name=form]").submit(function () {
+    var pokemon = $(this).find(':text[name="pokemon"]').val();
+    console.log(pokemon);
+    $.getJSON( "pokemons.json", function( data ) {
+    for (var i in data) {
+        if (data[i].name === pokemon) {
+            lowercasepokemon = pokemon.toLowerCase();
+            type = data[i].type;
+            replaceWith(lowercasepokemon, type)
+        }
+        else if (i === pokemon) {
+            namel = data[i].name;
+            type = data[i].type;
+            lowercasepokemon = namel.toLowerCase();
+            replaceWith(lowercasepokemon, type)
+        }
+    };
+})
+    return false;
+})
