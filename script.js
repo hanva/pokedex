7d1ebdac0c9a67974530510cadf23a5fca72b342
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-23 20:34:23 +0100

    image loaded

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-
-window.onload = function(){ 
+function replaceWith(name,type) {
+    $(".blackborder").replaceWith("<img src='http://img.pokemondb.net/artwork/" + name + ".jpg'>");
+    $("img").addClass("blackborder");
+    $("img").addClass("bigradius");
+    $(".type").html(name+type);
+}
+window.onload = function () {
     $("form[name=form]").submit(function () {
         var pokemon = $(this).find(':text[name="pokemon"]').val();
         console.log(pokemon);
@@ -8,24 +13,27 @@ window.onload = function(){
             type: "get",
             dataType: "json",
             success: function (data) {
+
                 for (var i in data) {
-                    if (data[i].name===pokemon){  
-                     ok = pokemon.toLowerCase();                   	
-                    $( ".blackborder" ).replaceWith( "<img src='http://img.pokemondb.net/artwork/"+ok+".jpg' class='blackborder'>" );
+                    if (data[i].name === pokemon) {
+                        lowercasepokemon = pokemon.toLowerCase();
+                        type = data[i].type;
+                        replaceWith(lowercasepokemon,type)
                     }
-                     else if(i===pokemon){
+
+                    else if (i === pokemon) {
                         namel = data[i].name;
-                        ok = namel.toLowerCase();
-                        $( ".blackborder" ).replaceWith( "<img src='http://img.pokemondb.net/artwork/"+ok+".jpg'" );
-                        $( ".blackborder" ).addClass("blackborder")
+                        type = data[i].type;
+                        lowercasepokemon = namel.toLowerCase();
+                        replaceWith(lowercasepokemon,type)
                     }
-                    };
-                
+
+                };
             },
             error: function () {
                 alert('oops');
             }
         })
-    return false;
+        return false;
     })
 }
