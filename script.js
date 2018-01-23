2303c9180e764da430a598d466ae454c5887e6ed
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-23 19:00:30 +0100

    adding a script when submiting

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 $.ajax({
     url: "pokemons.json",
-    type:"get",
-    dataType:"json",
-    success : function(data){
-        console.log(data)
+    type: "get",
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
+        for (var i in data) {
+            console.log(i)
+        }
     },
-    error : function(){
+    error: function () {
         alert('oops');
-    }}
+    }
+}
 )
+window.onload = function(){ 
+    $("form[name=form]").submit(function () {
+        var pokemon = $(this).find(':text[name="pokemon"]').val();
+    console.log(pokemon);
+    return false;
+    })
+}
