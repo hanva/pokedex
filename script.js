e14bc51342c59da9c9dfc94adad375af84d4e7ea
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-24 09:49:11 +0100

    nidoran

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $.ajax({
     url: "pokemons.json",
     type: "get",
     dataType: "json",
-    data :"data",
+    data: "data",
     success: "success"
 })
 function replaceWith(name, type) {
@@ -11,23 +11,37 @@ function replaceWith(name, type) {
     $("img").addClass("bigradius");
     $(".type").html(name + type);
 }
+function isValidPokemon(name) {
+    $.getJSON("pokemons.json", function (data) {
+        for (var i in data) {
+            if (data[i].name === name) {
+                return name
+            } 
+        }
+    })
+}
+var noice = isValidPokemon("Pikachu");
+console.log(noice)
 $("form[name=form]").submit(function () {
     var pokemon = $(this).find(':text[name="pokemon"]').val();
-    console.log(pokemon);
-    $.getJSON( "pokemons.json", function( data ) {
-    for (var i in data) {
-        if (data[i].name === pokemon) {
-            lowercasepokemon = pokemon.toLowerCase();
-            type = data[i].type;
-            replaceWith(lowercasepokemon, type)
-        }
-        else if (i === pokemon) {
-            namel = data[i].name;
-            type = data[i].type;
-            lowercasepokemon = namel.toLowerCase();
-            replaceWith(lowercasepokemon, type)
-        }
-    };
-})
+    if (pokemon < 1 || pokemon > 151) {
+        $(".error").html(pokemon + " is not a valid number");
+    }
+    $.getJSON("pokemons.json", function (data) {
+        for (var i in data) {
+            if (data[i].name === pokemon) {
+                lowercasepokemon = pokemon.toLowerCase();
+                type = data[i].type;
+                replaceWith(lowercasepokemon, type)
+                $(".error").html("");
+            } else if (i === pokemon) {
+                namel = data[i].name;
+                type = data[i].type;
+                lowercasepokemon = namel.toLowerCase();
+                replaceWith(lowercasepokemon, type)
+                $(".error").html("");
+            }
+        };
+    })
     return false;
 })
