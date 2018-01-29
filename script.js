bb118f8d513784b66d9166afddceffab98261812
Nicolas Tran <nicolas.tran@supinternet.fr>
2018-01-29 23:06:16 +0100

    indent

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-var data =$.ajax({
+var data = $.ajax({
     url: "pokemons.json",
     type: "get",
     dataType: "json",
     data: "data",
-    success: function(data){
+    success: function (data) {
         data = data
         return data
     }
 })
-function Premiere(chaine){
-    return chaine.substr(0,1).toUpperCase()+	chaine.substr(1,chaine.length).toLowerCase()
-}
 function replaceWith(name, type) {
     $(".blackborder").replaceWith("<img src='http://www.pokestadium.com/sprites/xy/" + name + ".gif'>");
     $("img").addClass("blackborder");
     $("img").addClass("bigradius");
-    $(".pokename").html("Name : "+name);
-    $(".poketype").html( "Type : "+type);
+    $(".pokename").html("Name : " + name);
+    $(".poketype").html("Type : " + type);
 }
-function replaceErrors(errornumb,errorname) {
-    $(".errornumb").html(errornumb)
-    $(".errorname").html(errorname)
+function replaceErrors(errors) {
+    $(".errors").html(errors)
 }
 $("form[name=form]").submit(function () {
     var pokemon = $(this).find(':text[name="pokemon"]').val();
     if (pokemon < 1 || pokemon > 151) {
-        replaceErrors(pokemon + ' is not a valid number',"")
+        replaceErrors('Number pokemon ' + pokemon + ' not found.')
         return false
     }
     pokemon = pokemon.substr(0, 1).toUpperCase() + pokemon.substr(1, pokemon.length);
@@ -35,18 +31,18 @@ $("form[name=form]").submit(function () {
                 lowercasepokemon = pokemon.toLowerCase();
                 type = data[i].type;
                 replaceWith(lowercasepokemon, type)
-                replaceErrors("","")
+                replaceErrors("")
                 return false
             } else if (i === pokemon) {
                 namel = data[i].name;
                 type = data[i].type;
                 lowercasepokemon = namel.toLowerCase();
                 replaceWith(lowercasepokemon, type)
-                replaceErrors("","")
+                replaceErrors("")
                 return false
             }
-            else{
-                replaceErrors("",pokemon+" is not a valid name")
+            else {
+                replaceErrors(pokemon + " not found")
             }
         };
     })
